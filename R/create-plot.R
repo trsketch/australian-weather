29134eab66252181f08e1e4f2ffb305a6b5d7e7d
trsketch <theo+sketch@jumpingrivers.com>
2021-01-19 11:01:41 +0000

    Adding y and x labels

--- R/create-plot.R
+++ R/create-plot.R
@@ -16,7 +16,9 @@ df %>%
   filter(Location == city) %>%
   ggplot(aes(x = Temp9am, y = Humidity9am)) + 
   geom_point() + 
-  labs(caption = "Australian Weathe")
+  labs(caption = "Australian Weathe",
+       x = "Temperature",
+       y = "Humidity")
 
 # Save the plot in graphics/
 ggsave(filename = "graphics/scatter-temp-humidity.png", plot = scatter_plot)
